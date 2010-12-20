1de85332088ec671e7be75df1dc526ddee6d6f79
mausch <mauricioscheffer@gmail.com>
2010-12-20 18:17:04 -0300

    refactored EnvDict to use (string*InputValue) list instead of dictionary

--- Formlets/Environ.fs
+++ Formlets/Environ.fs
@@ -8,16 +8,13 @@ type InputValue =
     | Value of string
     | File of HttpPostedFileBase
 
-type EnvDict = Dictionary<string, InputValue>
+type EnvDict = (string * InputValue) list
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module EnvDict =
-    let empty = EnvDict()
+    let empty = []
     let addFromSeq (l: (string*InputValue) seq) (d: EnvDict) =
-        let r = EnvDict(d)
-        for k,v in l do
-            r.[k] <- v
-        r
+        d @ Seq.toList l
     let addFromValueSeq (l: (string*string) seq) =
         l |> Seq.map (fun (k,v) -> k, Value v) |> addFromSeq
     let addFromNV (n: NameValueCollection) =
@@ -39,22 +36,31 @@ module Environ =
     let (<*>) f x = ap f x
     let lift f x : 'b Environ = puree f <*> x
     let lift2 f x y : 'c Environ = puree f <*> x <*> y
+
     let optionalLookup (n: string) : InputValue option Environ = 
-        fun (env: EnvDict) -> 
-            match env.TryGetValue n with
-            | true,x -> Some x
-            | _ -> None
+        let rec finder env =
+            match env with
+            | [] -> None
+            | x::xs -> 
+                let key,value = x
+                if key = n
+                    then Some value
+                    else finder xs
+        finder
+
     let lookup (n: string) : InputValue Environ =
         fun (env: EnvDict) ->
             match optionalLookup n env with
             | None -> failwithf "Key not found in environment: %s" n
             | Some v -> v
+
     let optionalLookupNonFile (n: string) : string option Environ =
         fun (env: EnvDict) ->
             match optionalLookup n env with
             | Some (Value x) -> Some x
             | Some (File _) -> failwithf "File not expected for key %s" n
             | None -> None
+
     let lookupNonFile (n: string) : string Environ =
         fun (env: EnvDict) ->
             match lookup n env with
