8b28ea1ba84c28937e84f220181455e2dbfc88fa
mausch <mauricioscheffer@gmail.com>
2011-01-13 18:00:30 -0300

    credit card validator

--- Formlets/Validators.fs
+++ Formlets/Validators.fs
@@ -12,3 +12,14 @@ module Validate =
     let notEmpty =
         let isOK = String.IsNullOrWhiteSpace >> not
         satisfies (err isOK (fun _ -> "Mandatory field"))
+
+    let creditCard =
+        // from http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#F.23
+        let luhn (s:string) =
+            let rec g r c = function
+            | 0 -> r
+            | i ->
+                let d = ((int s.[i - 1]) - 48) <<< c
+                g (r + if d < 10 then d else d - 9) (1 - c) (i - 1)
+            (g 0 0 s.Length) % 10 = 0
+        satisfies (err luhn (fun _ -> "Invalid credit card number"))
