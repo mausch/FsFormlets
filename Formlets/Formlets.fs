998bb885aec4eec97790fe5499fb25c91a5236a9
mausch <mauricioscheffer@gmail.com>
2011-01-13 00:28:50 -0300

    moved helpers to a separate module and file

--- Formlets/Formlets.fs
+++ Formlets/Formlets.fs
@@ -184,29 +184,6 @@ module Formlet =
         let v value = System.Text.RegularExpressions.Regex(rx).IsMatch(value)
         err v errorMsg
 
-    // helper functions
-
-    /// Builds a pair (2-tuple)
-    let t2 a b = a,b
-
-    /// Builds a triple (3-tuple)
-    let t3 a b c = a,b,c
-
-    /// Builds a 4-tuple
-    let t4 a b c d = a,b,c,d
-
-    /// Builds a 5-tuple
-    let t5 a b c d e = a,b,c,d,e
-
-    /// Builds a 6-tuple
-    let t6 a b c d e f = a,b,c,d,e,f
-
-    /// Builds a 7-tuple
-    let t7 a b c d e f g = a,b,c,d,e,f,g
-
-    /// Builds a 8-tuple
-    let t8 a b c d e f g h = a,b,c,d,e,f,g,h
-
     // Generic HTML functions
 
     let generalElement nameGen defaultValue (tag: string -> InputValue list -> xml_item list): InputValue list Formlet =
