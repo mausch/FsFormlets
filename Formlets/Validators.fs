39948f230d1dc4f0098022d1c2a7b2bf506409f7
mausch <mauricioscheffer@gmail.com>
2011-01-13 20:57:14 -0300

    moved to another module

--- Formlets/Validators.fs
+++ Formlets/Validators.fs
@@ -2,6 +2,17 @@
 
 open System
 
+module Validators = 
+    // from http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#F.23
+    let luhn (s:string) =
+        let rec g r c = function
+        | 0 -> r
+        | i ->
+            let d = ((int s.[i - 1]) - 48) <<< c
+            g (r + if d < 10 then d else d - 9) (1 - c) (i - 1)
+        (g 0 0 s.Length) % 10 = 0
+
+
 module Validate =
     let isInt =
         let isOK = Int32.TryParse >> fst
@@ -14,12 +25,4 @@ module Validate =
         satisfies (err isOK (fun _ -> "Mandatory field"))
 
     let creditCard =
-        // from http://rosettacode.org/wiki/Luhn_test_of_credit_card_numbers#F.23
-        let luhn (s:string) =
-            let rec g r c = function
-            | 0 -> r
-            | i ->
-                let d = ((int s.[i - 1]) - 48) <<< c
-                g (r + if d < 10 then d else d - 9) (1 - c) (i - 1)
-            (g 0 0 s.Length) % 10 = 0
-        satisfies (err luhn (fun _ -> "Invalid credit card number"))
+        satisfies (err Validators.luhn (fun _ -> "Invalid credit card number"))
