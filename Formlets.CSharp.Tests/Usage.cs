e2185c241918152845cca29f9f5aebfc45f6d9a9
mausch <mauricioscheffer@gmail.com>
2011-01-20 20:48:28 -0300

    test running a formlet in C#

--- Formlets.CSharp.Tests/Usage.cs
+++ Formlets.CSharp.Tests/Usage.cs
@@ -6,8 +6,13 @@ using Xunit;
 namespace Formlets.CSharp.Tests {
     public class Usage {
         [Fact]
-        public void Test() {
+        public void Run() {
             var input = Formlet.Input("", Enumerable.Empty<KeyValuePair<string,string>>());
+            var result = input.Run(new Dictionary<string, string> { { "input_0", "something" } });
+            Assert.True(result.Value.IsSome());
+            Assert.Equal("something", result.Value.Value);
+            Console.WriteLine(result.ErrorForm);
+            Assert.Equal("<input name=\"input_0\" value=\"something\" />", result.ErrorForm.ToString());
         }
     }
 }
---
+++ Formlets.CSharp/FSharpOptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.FSharp.Core;
+
+namespace Formlets.CSharp {
+    public static class FSharpOptionExtensions {
+        public static bool IsSome<T>(this FSharpOption<T> option) {
+            return FSharpOption<T>.get_IsSome(option);
+        }
+
+        public static bool IsNone<T>(this FSharpOption<T> option) {
+            return FSharpOption<T>.get_IsNone(option);
+        }
+    }
+}
