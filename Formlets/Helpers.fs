b51c63de5268186390edfa95bf519048baa979e0
mausch <mauricioscheffer@gmail.com>
2011-01-22 17:11:47 -0300

    inlining

--- Formlets/Helpers.fs
+++ Formlets/Helpers.fs
@@ -9,13 +9,13 @@ module NameValueCollection =
         x.Add b
         x
 
-    let add (x: NameValueCollection) (a,b) = x.Add(a,b)
+    let inline add (x: NameValueCollection) (a,b) = x.Add(a,b)
 
     let toSeq (a: NameValueCollection) =
         a.AllKeys
         |> Seq.collect (fun k -> a.GetValues k |> Seq.map (fun v -> k,v))
 
-    let toList a = toSeq a |> Seq.toList
+    let inline toList a = toSeq a |> Seq.toList
 
     let fromSeq l =
         let x = NameValueCollection()
@@ -44,25 +44,25 @@ module Helpers =
     let inline hashs a = (hash a).ToString()
 
     /// Builds a pair (2-tuple)
-    let t2 a b = a,b
+    let inline t2 a b = a,b
 
     /// Builds a triple (3-tuple)
-    let t3 a b c = a,b,c
+    let inline t3 a b c = a,b,c
 
     /// Builds a 4-tuple
-    let t4 a b c d = a,b,c,d
+    let inline t4 a b c d = a,b,c,d
 
     /// Builds a 5-tuple
-    let t5 a b c d e = a,b,c,d,e
+    let inline t5 a b c d e = a,b,c,d,e
 
     /// Builds a 6-tuple
-    let t6 a b c d e f = a,b,c,d,e,f
+    let inline t6 a b c d e f = a,b,c,d,e,f
 
     /// Builds a 7-tuple
-    let t7 a b c d e f g = a,b,c,d,e,f,g
+    let inline t7 a b c d e f g = a,b,c,d,e,f,g
 
     /// Builds a 8-tuple
-    let t8 a b c d e f g h = a,b,c,d,e,f,g,h
+    let inline t8 a b c d e f g h = a,b,c,d,e,f,g,h
 
     let appendToSameKey key sep value attr =
         match Seq.tryFindWithIndex (fun (k,_) -> k = key) attr with
