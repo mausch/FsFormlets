2fe8a0815dceecd9afb63ec64b86670678c608f6
mausch <mauricioscheffer@gmail.com>
2011-01-24 18:50:19 -0300

    more partial active patterns for xml linq elements

--- Formlets/XmlWriter.fs
+++ Formlets/XmlWriter.fs
@@ -6,18 +6,69 @@ type 'a XmlWriter = XNode list * 'a
 
 [<AutoOpen>]
 module XmlHelpers =
-    let (|Tag|_|) (n: XNode) = 
-        match n with
-        | :? XElement as e -> Some e
-        | _ -> None
-    let (|Text|_|) (n: XNode) =
-        match n with
-        | :? XText as t -> Some t
-        | _ -> None
+    /// Gets attributes of an element as a tuple list
     let getAttr (e: XElement) =
         e.Attributes() 
         |> Seq.map (fun a -> a.Name.LocalName,a.Value)
         |> Seq.toList
+
+    /// <summary>
+    /// Matches a <see cref="XElement"/>
+    /// </summary>
+    /// <param name="n"></param>
+    let (|Tag|_|) (n: XNode) = 
+        match n with
+        | :? XElement as e -> Some e
+        | _ -> None
+
+    /// <summary>
+    /// Matches a <see cref="XElement"/>, splitting name, attributes and children
+    /// </summary>
+    /// <param name="n"></param>
+    let (|TagA|_|) (n : XNode) =
+        match n with
+        | Tag t -> 
+            let name = t.Name.LocalName
+            let attr = getAttr t
+            let children = t.Nodes() |> Seq.toList
+            Some(name,attr,children)
+        | _ -> None
+
+    /// <summary>
+    /// Matches a <see cref="XText"/>
+    /// </summary>
+    /// <param name="n"></param>
+    let (|Text|_|) (n: XNode) =
+        match n with
+        | :? XText as t -> Some t
+        | _ -> None
+
+    /// <summary>
+    /// Matches a <see cref="XText"/>, extracting the actual text value
+    /// </summary>
+    /// <param name="n"></param>
+    let (|TextV|_|) (n: XNode) =
+        match n with
+        | Text t -> Some t.Value
+        | _ -> None
+
+    /// <summary>
+    /// Matches a <see cref="XComment"/>
+    /// </summary>
+    /// <param name="n"></param>
+    let (|Comment|_|) (n: XNode) =
+        match n with
+        | :? XComment as c -> Some c
+        | _ -> None
+
+    /// <summary>
+    /// Matches a <see cref="XComment"/>, extract the actual text value
+    /// </summary>
+    /// <param name="n"></param>
+    let (|CommentV|_|) (n: XNode) =
+        match n with
+        | Comment c -> Some c.Value
+        | _ -> None
 
 /// Applicative functor that manipulates HTML as XML
 module XmlWriter =
