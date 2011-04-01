f178cf3cf70a0473505b5005316a737e5caaf741
mausch <mauricioscheffer@gmail.com>
2011-04-01 16:54:49 -0300

    assemblyinfo

---
+++ Formlets/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace FsFormlets
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("FsFormlets")>]
+[<assembly: AssemblyDescription("Formlets")>]
+[<assembly: AssemblyProduct("FsFormlets")>]
+[<assembly: AssemblyCopyright("Copyright Mauricio Scheffer 2011")>]
+do()
